048aa6ace2ab53f762e415ae8b708f4ddadd531d
PatrykJNowak <patryk4820@gmail.com>
2022-08-22 13:16:23 +0200

    Contex db update

--- UserCarDealer/DataModels/SellData.cs
+++ UserCarDealer/DataModels/SellData.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections.Generic;
 using Microsoft.VisualBasic;
+using Npgsql.PostgresTypes;
 
 namespace UserCarDealer.DataModels
 {
     public class SellData
     {
         public int Id { get; set; }
-        // public Customer CustomerId { get; set; }
-        // public Vehicle VehicleId { get; set; }
+        public int CustomerId { get; set; }
+        public int VehicleId { get; set; }
+        // public ICollection<Customer> CustomerId { get; set; }
+        // public ICollection<Vehicle> VehicleId { get; set; }
         public string Date { get; set; }
     }
 }
